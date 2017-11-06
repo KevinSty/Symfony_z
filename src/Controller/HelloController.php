847344b0c8a17b8e39226077d064d21ab2d9647b
kevin.diaz <kevin.diaz@info0726.iut-lens.univ-artois.fr>
2017-11-06 15:50:56 +0100

    menu en cours

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloController extends Controller {
+
+    /**
+     * @Route("/hello", name="hello")
+     */
+    function hello() {
+
+        return $this->render('Hello/hello.html.twig');
+    }
+
+    /**
+     * @Route("/hello/menu", name="menu")
+     */
+    function menu() {
+        return $this->render('Page/menu.html.twig');
+    }
+
+    /**
+     * @Route("/hello/page1", name="page1")
+     */
+    function page1() {
+        return $this->render('Hello/page1.html.twig');
+    }
+
+    /**
+     * @Route("/hello/page2", name="page2")
+     */
+    function page2() {
+        return $this->render('Hello/page2.html.twig');
+    }
+
+}
